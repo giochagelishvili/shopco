e000d7dcb43d57d0a4b2cc13f56036cec99d1078
Giorgi Chagelishvili <chagx116@gmail.com>
2023-09-14 16:07:33 +0400

    put footer in separate file

--- shop/index.php
+++ shop/index.php
@@ -185,75 +185,7 @@
         </section>
     </main>
 
-    <footer class="p-4 bg-[#F2F0F1]">
-        <div>
-            <h1 class="font-bebas font-bold text-6xl mt-1">SHOP.CO</h1>
-            <p class="opacity-60">
-                We have clothes that suits your style and which you’re proud to wear. From women to men.
-            </p>
-        </div>
-
-        <div class="my-6 flex items-strech gap-6 text-3xl">
-            <i class="fa-brands fa-x-twitter"></i>
-            <i class="fa-brands fa-facebook-f"></i>
-            <i class="fa-brands fa-instagram"></i>
-        </div>
-
-        <div class="grid grid-cols-2 gap-y-8 w-full">
-            <div>
-                <h1 class="font-raleway font-semibold text-lg">COMPANY</h1>
-                <ul class="flex flex-col items-start gap-2 mt-2 text-gray-500">
-                    <li><a href="#">About</a></li>
-                    <li><a href="#">Features</a></li>
-                    <li><a href="#">Works</a></li>
-                    <li><a href="#">Career</a></li>
-                </ul>
-            </div>
-
-            <div>
-                <h1 class="font-raleway font-semibold text-lg">HELP</h1>
-                <ul class="flex flex-col items-start gap-2 mt-2 text-gray-500">
-                    <li><a href="#">Customer Support</a></li>
-                    <li><a href="#">Delivery Details</a></li>
-                    <li><a href="#">Terms & Conditions</a></li>
-                    <li><a href="#">Privacy Policy</a></li>
-                </ul>
-            </div>
-
-            <div>
-                <h1 class="font-raleway font-semibold text-lg">FAQ</h1>
-                <ul class="flex flex-col items-start gap-2 mt-2 text-gray-500">
-                    <li><a href="#">Account</a></li>
-                    <li><a href="#">Manage Deliveries</a></li>
-                    <li><a href="#">Orders</a></li>
-                    <li><a href="#">Payment</a></li>
-                </ul>
-            </div>
-
-            <div>
-                <h1 class="font-raleway font-semibold text-lg">Resources</h1>
-                <ul class="flex flex-col items-start gap-2 mt-2 text-gray-500">
-                    <li><a href="#">Free eBook</a></li>
-                    <li><a href="#">Development Tutorial</a></li>
-                    <li><a href="#">How to - Blog</a></li>
-                    <li><a href="#">Youtube Playlist</a></li>
-                </ul>
-            </div>
-        </div>
-
-        <div class="h-[0.5px] w-full bg-gray-400 my-8"></div>
-
-        <div class="flex flex-col items-center justify-between">
-            <h2 class="font-raleway opacity-70 text-sm">Shop.co © 2000-2023, All Rights Reserved</h2>
-            <div class="flex items-center gap-4 my-4">
-                <i class="fa-brands fa-cc-visa text-3xl"></i>
-                <i class="fa-brands fa-cc-mastercard text-3xl"></i>
-                <i class="fa-brands fa-cc-paypal text-3xl"></i>
-                <i class="fa-brands fa-cc-apple-pay text-3xl"></i>
-                <i class="fa-brands fa-google-pay text-3xl"></i>
-            </div>
-        </div>
-    </footer>
+    <?php include("../inc/footer.php") ?>
 </body>
 
 </html>
