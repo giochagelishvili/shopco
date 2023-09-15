da5a72df729c6074b2bb0c4dcad2fe75ab337e22
Giorgi Chagelishvili <chagx116@gmail.com>
2023-09-15 13:17:15 +0400

    add responsive header

--- inc/header.php
+++ inc/header.php
@@ -1,25 +1,46 @@
 <header>
     <div class="flex items-center justify-between py-3 px-6">
-        <div class="flex items-center gap-4">
-            <span class="material-symbols-outlined text-4xl menu-btn">
+        <div class="flex items-center gap-4 xl:gap-28">
+            <span class="material-symbols-outlined text-4xl menu-btn xl:hidden">
                 menu
             </span>
-            <h1 class="font-bebas text-3xl mt-1">SHOP.CO</h1>
+            <h1 class="font-bebas text-3xl mt-1 xl:text-5xl">SHOP.CO</h1>
+
+            <div class="hidden xl:block">
+                <nav class="h-full">
+                    <ul class="flex items-center justify-between gap-12 text-xl font-raleway">
+                        <li><a href="#">Shop</a></li>
+                        <li><a href="#">On Sale</a></li>
+                        <li><a href="#">New Arrivals</a></li>
+                        <li><a href="#">Brands</a></li>
+                    </ul>
+                </nav>
+            </div>
         </div>
 
         <div class="flex items-center gap-4">
-            <span class="material-symbols-outlined text-3xl">
+            <span class="material-symbols-outlined text-3xl xl:hidden">
                 search
             </span>
+
+            <div class="hidden xl:flex xl:items-center xl:justify-start xl:gap-4 xl:bg-[#F0F0F0] xl:py-2 xl:px-4 xl:w-96 xl:rounded-full">
+                <span class="material-symbols-outlined text-3xl">
+                    search
+                </span>
+                <input type="text" name="search-bar" id="search-bar" placeholder="Search for products..." class="bg-transparent w-full" />
+            </div>
+
             <span class="material-symbols-outlined text-3xl">
                 shopping_cart
             </span>
+
             <span class="material-symbols-outlined text-3xl">
                 account_circle
             </span>
         </div>
     </div>
-    <div class="nav-menu bg-black text-white w-3/4 h-screen fixed top-0 right-full py-6 pl-8">
+
+    <div class="nav-menu bg-black text-white w-3/4 h-screen fixed top-0 right-full py-6 pl-8 xl:hidden">
         <h1 class="font-bebas text-4xl tracking-widest">MENU</h1>
         <nav class="relative">
             <ul class="mt-5 text-xl">
