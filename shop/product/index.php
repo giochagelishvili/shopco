ca1604777d843228ed678d83b686dd78280f04aa
Giorgi Chagelishvili <chagx116@gmail.com>
2023-09-20 21:02:58 +0400

    add desktop responsive

--- shop/product/index.php
+++ shop/product/index.php
@@ -61,106 +61,112 @@ if (!isset($_GET['id'])) {
 
                 <!-- Product div -->
                 <div class="product-container p-4">
-                    <!-- This div contains images of the product -->
-                    <div class="img-container">
-                        <!-- Load main image -->
-                        <div>
-                            <img class="main-img" src=<?php echo "/shopco/uploads/product_images/" . $product_images[0] ?> alt="product image">
+                    <!-- Product image and product info container -->
+                    <div class="xl:flex xl:items-center xl:justify-start gap-6">
+                        <!-- This div contains images of the product -->
+                        <div class="img-container xl:flex xl:flex-row-reverse xl:items-center xl:justify-start xl:h-[400px] xl:gap-4">
+                            <!-- Load main image -->
+                            <div>
+                                <img class="main-img" src=<?php echo "/shopco/uploads/product_images/" . $product_images[0] ?> alt="product image">
+                            </div>
+
+                            <!-- Secondary images container -->
+                            <div class="flex items-center gap-2 mt-4 overflow-x-scroll xl:overflow-x-visible xl:flex-col xl:!h-full xl:overflow-y-scroll xl:m-0 secondary-images-container">
+                                <?php
+                                // Loop through images and display them on the page
+                                foreach ($product_images as $product_image) {
+                                    echo "<img class='w-1/3 xl:w-40 secondary-img' src='/shopco/uploads/product_images/" . $product_image . "' alt='product image'>";
+                                }
+                                ?>
+                            </div>
                         </div>
 
-                        <!-- Secondary images container -->
-                        <div class="flex items-center gap-2 overflow-x-scroll mt-4">
-                            <?php
-                            // Loop through images and display them on the page
-                            foreach ($product_images as $product_image) {
-                                echo "<img class='w-1/3 secondary-img' src='/shopco/uploads/product_images/" . $product_image . "' alt='product image'>";
-                            }
-                            ?>
-                        </div>
-                    </div>
+                        <!-- Product info container -->
+                        <div class="xl:flex-1">
+                            <!-- This div contains name of the product -->
+                            <div class="name-container my-2 w-2/3 xl:my-1">
+                                <h1 class="font-bebas font-bold text-4xl tracking-wide">
+                                    <?php echo $product_name ?>
+                                </h1>
+                            </div>
 
-                    <!-- This div contains name of the product -->
-                    <div class="name-container my-2 w-2/3">
-                        <h1 class="font-bebas font-bold text-4xl tracking-wide">
-                            <?php echo $product_name ?>
-                        </h1>
-                    </div>
+                            <!-- This div contains rating of the product -->
+                            <div class="rating-div flex items-center gap-2">
+                                <?php echo $product_rating ?>
+                            </div>
 
-                    <!-- This div contains rating of the product -->
-                    <div class="rating-div flex items-center gap-2">
-                        <?php echo $product_rating ?>
-                    </div>
+                            <!-- This div contains the price of the product -->
+                            <div class="price-div font-raleway font-bold text-3xl my-2 xl:my-1">
+                                $<?php echo $product_price ?>
+                            </div>
 
-                    <!-- This div contains the price of the product -->
-                    <div class="price-div font-raleway font-bold text-3xl my-2">
-                        $<?php echo $product_price ?>
-                    </div>
+                            <!-- Divider line -->
+                            <hr class="my-6 xl:my-4">
 
-                    <!-- Divider line -->
-                    <hr class="my-6">
+                            <!-- This div contains colors for selection -->
+                            <div>
+                                <h2 class="font-raleway opacity-70">Select Colors</h2>
 
-                    <!-- This div contains colors for selection -->
-                    <div>
-                        <h2 class="font-raleway opacity-70">Select Colors</h2>
+                                <!-- Colors container -->
+                                <div class="flex items-center gap-2 mt-2 xl:mt-1">
+                                    <?php
+                                    // Loop through colors and display them on the page
+                                    foreach ($colors as $color) {
+                                        // Color div
+                                        echo "<div class='flex items-center justify-center relative color-container cursor-pointer'>";
+                                        echo "<img src='/shopco/uploads/colors/$color' />"; // Color icon
+                                        echo "<span class='material-symbols-outlined text-white absolute z-10 hidden tick-mark'>done</span>"; // Checkmark icon
+                                        echo "</div>"; // End of color div
+                                    }
+                                    ?>
+                                </div>
+                            </div>
 
-                        <!-- Colors container -->
-                        <div class="flex items-center gap-2 mt-2">
-                            <?php
-                            // Loop through colors and display them on the page
-                            foreach ($colors as $color) {
-                                // Color div
-                                echo "<div class='flex items-center justify-center relative color-container cursor-pointer'>";
-                                echo "<img src='/shopco/uploads/colors/$color' />"; // Color icon
-                                echo "<span class='material-symbols-outlined text-white absolute z-10 hidden tick-mark'>done</span>"; // Checkmark icon
-                                echo "</div>"; // End of color div
-                            }
-                            ?>
-                        </div>
-                    </div>
+                            <!-- Divider line -->
+                            <hr class="my-6 xl:my-4">
 
-                    <!-- Divider line -->
-                    <hr class="my-6">
+                            <!-- This div contains size for selection -->
+                            <div>
+                                <h2 class="font-raleway opacity-70">Choose Size</h2>
 
-                    <!-- This div contains size for selection -->
-                    <div>
-                        <h2 class="font-raleway opacity-70">Choose Size</h2>
+                                <!-- Size container -->
+                                <div class="flex items-center gap-2 flex-wrap mt-2 size-container xl:mt-1">
+                                    <?php
+                                    // Loop through sizes and display them on the page
+                                    foreach ($sizes as $size) {
+                                        echo "<button class='bg-gray-200 py-2 px-5 rounded-full font-raleway size-btn'>$size</button>";
+                                    }
+                                    ?>
+                                </div>
+                            </div>
 
-                        <!-- Size container -->
-                        <div class="flex items-center gap-2 flex-wrap mt-2 size-container">
-                            <?php
-                            // Loop through sizes and display them on the page
-                            foreach ($sizes as $size) {
-                                echo "<button class='bg-gray-200 py-2 px-5 rounded-full font-raleway size-btn'>$size</button>";
-                            }
-                            ?>
-                        </div>
-                    </div>
+                            <!-- Divider line -->
+                            <hr class="my-6 xl:my-4">
 
-                    <!-- Divider line -->
-                    <hr class="my-6">
+                            <!-- This div contains quantity and add to cart buttons -->
+                            <div class="flex items-center gap-4">
+                                <!-- Quantity container -->
+                                <div class="flex items-center gap-3 bg-gray-200 px-5 py-2 rounded-full font-raleway text-2xl">
+                                    <!-- Minus button -->
+                                    <span class="material-symbols-outlined minus-btn cursor-pointer">
+                                        remove
+                                    </span>
 
-                    <!-- This div contains quantity and add to cart buttons -->
-                    <div class="flex items-center gap-4">
-                        <!-- Quantity container -->
-                        <div class="flex items-center gap-3 bg-gray-200 px-5 py-2 rounded-full font-raleway text-2xl">
-                            <!-- Minus button -->
-                            <span class="material-symbols-outlined minus-btn">
-                                remove
-                            </span>
+                                    <!-- Quantity -->
+                                    <span class="quantity">1</span>
 
-                            <!-- Quantity -->
-                            <span class="quantity">1</span>
+                                    <!-- Plus button -->
+                                    <span class="material-symbols-outlined cursor-pointer plus-btn">
+                                        add
+                                    </span>
+                                </div>
 
-                            <!-- Plus button -->
-                            <span class="material-symbols-outlined plus-btn">
-                                add
-                            </span>
-                        </div>
-
-                        <!-- Add to Cart button container -->
-                        <div class="w-full">
-                            <!-- Add to Cart button -->
-                            <button class='bg-black text-white py-3 w-full rounded-full font-raleway size-btn'>Add to Cart</button>
+                                <!-- Add to Cart button container -->
+                                <div class="w-full">
+                                    <!-- Add to Cart button -->
+                                    <button class='bg-black text-white py-3 w-full rounded-full font-raleway size-btn'>Add to Cart</button>
+                                </div>
+                            </div>
                         </div>
                     </div>
 
@@ -223,7 +229,7 @@ if (!isset($_GET['id'])) {
                         <div>
                             <h1 class="font-bebas font-bold text-5xl text-center">YOU MIGHT ALSO LIKE</h1>
                         </div>
-                        <div class="flex items-center gap-4 overflow-x-scroll my-8">
+                        <div class="flex items-center gap-4 overflow-x-scroll my-8 xl:overflow-visible">
                             <?php
                             foreach ($similar_products as $similar_product) {
                                 $product_id = $similar_product["id"]; // Product ID
@@ -246,24 +252,27 @@ if (!isset($_GET['id'])) {
                         </div>
                     </div>
 
-                    <div class="bg-black mt-12 flex flex-col items-center justify-center gap-4 p-8 rounded-3xl xl:m-0 xl:w-1/3">
-                        <h1 class="font-bebas font-semibold text-white text-5xl ">
-                            STAY UP TO DATE <br>
-                            ABOUT OUR <br>
-                            LATEST OFFERS
-                        </h1>
+                    <!-- Subscription container -->
+                    <div class="xl:flex xl:items-center xl:justify-center xl:w-full">
+                        <div class="bg-black mt-12 flex flex-col items-center justify-center gap-4 p-8 rounded-3xl xl:m-0 xl:flex-row xl:w-4/5 xl:gap-8">
+                            <h1 class="font-bebas font-semibold text-white text-5xl ">
+                                STAY UP TO DATE <br>
+                                ABOUT OUR <br>
+                                LATEST OFFERS
+                            </h1>
 
-                        <form action="" class="w-full">
-                            <div class="bg-white flex items-center gap-2 py-1 pl-4 rounded-full">
-                                <span class="material-symbols-outlined">
-                                    mail
-                                </span>
-                                <input type="email" name="email-input" id="email-input" placeholder="Enter your email address" class="py-3" />
-                            </div>
-                            <div class="flex items-center">
-                                <button type="submit" class="w-full font-raleway font-semibold text-center text-md py-3 rounded-full bg-white mt-4 transition-all hover:text-white hover:bg-green-600">Subscribe to newsletter</button>
-                            </div>
-                        </form>
+                            <form action="" class="w-full xl:w-2/3">
+                                <div class="bg-white flex items-center gap-2 py-1 pl-4 rounded-full">
+                                    <span class="material-symbols-outlined">
+                                        mail
+                                    </span>
+                                    <input type="email" name="email-input" id="email-input" placeholder="Enter your email address" class="py-3" />
+                                </div>
+                                <div class="flex items-center">
+                                    <button type="submit" class="w-full font-raleway font-semibold text-center text-md py-3 rounded-full bg-white mt-4 transition-all hover:text-white hover:bg-green-600">Subscribe to newsletter</button>
+                                </div>
+                            </form>
+                        </div>
                     </div>
                 </div>
             </div>
